65273e7313304054f0a5ece432c256eef864602c
ericthecurious <hello@ericthecurious.com>
2025-07-25 12:10:21 -0700

    patch: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-//exports go here
+export { default as WaveshareRoboticArm } from './modules/WaveshareRoboticArm'
+export * from './modules/WaveshareRoboticArm'
+
+export { default as FakeRoboticArm } from './testDoubles/FakeRoboticArm'
+export * from './testDoubles/FakeRoboticArm'
+
+export { default as FakeAxios } from './testDoubles/FakeAxios'
+export * from './testDoubles/FakeAxios'
