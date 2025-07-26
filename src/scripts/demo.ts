5ab7e687c522291b826450e1a6b88eb7c7527643
ericthecurious <hello@ericthecurious.com>
2025-07-25 17:51:37 -0700

    patch: slow down speeds

--- src/scripts/demo.ts
+++ src/scripts/demo.ts
@@ -3,10 +3,10 @@ import WaveshareRoboticArm from '../modules/WaveshareRoboticArm'
 async function main() {
     const instance = await WaveshareRoboticArm.Create()
 
-    await instance.moveTo({ x: 200, y: 0, z: -50, spd: 0.25 })
+    await instance.moveTo({ x: 200, y: 0, z: -50, spd: 0.1 })
     await instance.moveTo({ x: 250, y: 0, z: -50, spd: 0.1 })
-    await instance.moveTo({ x: 250, y: 0, z: -20, spd: 0.25 })
-    await instance.moveTo({ x: 200, y: 0, z: -20, spd: 0.25 })
+    await instance.moveTo({ x: 250, y: 0, z: -20, spd: 0.1 })
+    await instance.moveTo({ x: 200, y: 0, z: -20, spd: 0.1 })
 }
 
 main().catch((error) => {
