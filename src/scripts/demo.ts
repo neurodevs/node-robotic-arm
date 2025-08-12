db073f1c2401060afee814e1cc3f592fd8600394
ericthecurious <hello@ericthecurious.com>
2025-08-12 01:31:06 -0700

    patch: update demo

--- src/scripts/demo.ts
+++ src/scripts/demo.ts
@@ -3,10 +3,29 @@ import WaveshareRoboticArm from '../modules/WaveshareRoboticArm'
 async function main() {
     const instance = await WaveshareRoboticArm.Create()
 
-    await instance.moveTo({ x: 200, y: 0, z: -50, spd: 0.1 })
-    await instance.moveTo({ x: 250, y: 0, z: -50, spd: 0.1 })
-    await instance.moveTo({ x: 250, y: 0, z: -20, spd: 0.1 })
-    await instance.moveTo({ x: 200, y: 0, z: -20, spd: 0.1 })
+    await instance.moveTo({ x: 200, y: -300, z: -50, spd: 0.3 })
+    await instance.moveTo({ x: 200, y: -300, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: -280, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: -280, z: -50, spd: 0.1 })
+    await instance.moveTo({ x: 200, y: -300, z: -50, spd: 0.1 })
+
+    await instance.moveTo({ x: 200, y: 300, z: -50, spd: 0.3 })
+    await instance.moveTo({ x: 200, y: 300, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: 280, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: 280, z: -50, spd: 0.1 })
+    await instance.moveTo({ x: 200, y: 300, z: -50, spd: 0.1 })
+
+    await instance.moveTo({ x: 200, y: -300, z: -50, spd: 0.3 })
+    await instance.moveTo({ x: 200, y: -300, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: -280, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: -280, z: -50, spd: 0.1 })
+    await instance.moveTo({ x: 200, y: -300, z: -50, spd: 0.1 })
+
+    await instance.moveTo({ x: 200, y: 300, z: -50, spd: 0.3 })
+    await instance.moveTo({ x: 200, y: 300, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: 280, z: -110, spd: 0.1 })
+    await instance.moveTo({ x: 250, y: 280, z: -50, spd: 0.1 })
+    await instance.moveTo({ x: 200, y: 300, z: -50, spd: 0.1 })
 }
 
 main().catch((error) => {
